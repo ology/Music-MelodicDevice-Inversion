bb53a71dea9310f267e9e17c52d61808b0062f2e
Gene Boggs <gene@ology.net>
2020-12-15 07:36:12 -0800

    Bump version

--- lib/Music/MelodicDevice/Inversion.pm
+++ lib/Music/MelodicDevice/Inversion.pm
@@ -2,7 +2,7 @@ package Music::MelodicDevice::Inversion;
 
 # ABSTRACT: Apply melodic inversion to a series of notes
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0102';
 
 use Data::Dumper::Compact qw(ddc);
 use List::SomeUtils qw(first_index);
